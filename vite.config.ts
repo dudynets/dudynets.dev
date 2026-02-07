fd464014edd4072712e780ee6a14c86f79dc47e7
Oleksandr Dudynets <hello@dudynets.dev>
2026-02-07 23:01:09 +0200

    refactor: extract page sections into HTML templates

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import {defineConfig} from 'vite';
 import {imageOptimizer} from './plugins/image-optimizer/index.js';
 import htmlMinifier from 'vite-plugin-html-minifier';
 import autoprefixer from 'autoprefixer';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig({
   server: {allowedHosts: true},
-  plugins: [imageOptimizer(), htmlMinifier({minify: true})],
+  plugins: [injectHTML(), imageOptimizer(), htmlMinifier({minify: true})],
   css: {
     postcss: {
       plugins: [autoprefixer()],
